b25ca30d2052f7a7e316228afde64c1a3b2a18de
HaroldasT2 <D00256351@student.dkit.ie>
2025-02-06 11:55:23 +0000

    Cleaned Up the Backend 3 Cleaned up the following: - userController.js & seeder.js (files), removed unique id for the user

--- backend/seeder.js
+++ backend/seeder.js
@@ -11,13 +11,11 @@ connectDB();
 
 const users = [
   {
-    id: 1,
     name: "Admin User",
     email: "admin@email.com",
     password: bcrypt.hashSync("123456", 10), 
   },
   {
-    id: 2,
     name: "John Doe",
     email: "john@email.com",
     password: bcrypt.hashSync("password", 10),
