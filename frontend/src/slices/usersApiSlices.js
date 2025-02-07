1faefd38db722b3ed939a24729df00e2dd674eb1
JdcStig <duffycreagh2003@gmail.com>
2025-02-07 16:15:32 +0000

    Updated the Frontend Folder to contain the following:
    Trying to get deploy working
    
    Updated
    server , userRoutes , apiSlice and usersApiSclice

--- frontend/src/slices/usersApiSlices.js
+++ frontend/src/slices/usersApiSlices.js
@@ -1,36 +1,41 @@
-import { USERS_URL } from "../constants";
-import { apiSlice } from "./apiSlice";
+import { USERS_URL } from '../constants';
+import { apiSlice } from './apiSlice';
 
 export const usersApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     login: builder.mutation({
       query: (data) => ({
-        url: `${USERS_URL}/auth`,
-        method: "POST",
+        url: `${USERS_URL}/auth`, // Will resolve to "/api/users/auth"
+        method: 'POST',
         body: data,
       }),
     }),
     googleLogin: builder.mutation({
       query: (data) => ({
-        url: `${USERS_URL}/google-auth`,
-        method: "POST",
+        url: `${USERS_URL}/google-auth`, // Resolves to "/api/users/google-auth"
+        method: 'POST',
         body: data,
       }),
     }),
     register: builder.mutation({
       query: (data) => ({
-        url: `${USERS_URL}`,
-        method: "POST",
+        url: `${USERS_URL}`, // Resolves to "/api/users"
+        method: 'POST',
         body: data,
       }),
     }),
     logout: builder.mutation({
       query: () => ({
-        url: `${USERS_URL}/logout`,
-        method: "POST",
+        url: `${USERS_URL}/logout`, // Resolves to "/api/users/logout"
+        method: 'POST',
       }),
     }),
   }),
 });
 
-export const { useLoginMutation, useGoogleLoginMutation, useRegisterMutation, useLogoutMutation } = usersApiSlice;
+export const {
+  useLoginMutation,
+  useGoogleLoginMutation,
+  useRegisterMutation,
+  useLogoutMutation,
+} = usersApiSlice;
